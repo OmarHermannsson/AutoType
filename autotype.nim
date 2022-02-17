18dd19dec1c51680d5ce6d1460848caf2b55d3d4
Omar Hermannsson <omarhem@gmail.com>
2022-02-17 20:17:19 +0000

    Better VMRC functionality
    VMRC and a number of other virtual consoles don't work with unicode sendinput, only with scancodes.
    
    Switched to use scancodes by default and falling back to unicode input when we can't deduce the scancode.
    
    Also we will now try to click in the middle of the VMRC window so the console should get the keyboard input.

--- autotype.nim
+++ autotype.nim
@@ -31,6 +31,7 @@ import std/tables
 import std/os
 import wNim/[wApp, wFrame, wPanel, wStaticText, wTextCtrl, wButton, wMenuBar, wMessageDialog, wIcon]
 import winim/lean
+import winim/inc/[windef, winbase, winuser]
 
 # For the binary icon
 {.link: "resource/icon.res".}
@@ -44,8 +45,14 @@ let label = StaticText(panel, label="Text value to type:")
 let input = TextCtrl(panel, style=wTePassword)
 let button = Button(panel, label="Select &window...")
 
+type 
+  WindowObj = object
+    hwnd: HWND
+    className: string
+    rect: RECT
+
 let autoMenu = Menu()
-var autoKey: Table[string, HWND]
+var autoKey: Table[string, WindowObj]
 
 proc layout() =
   panel.autolayout """
@@ -53,24 +60,93 @@ proc layout() =
     V:|-[line]-|
   """
 
+proc sendKey(vkey:SHORT, sendKeyUp:bool) =
+  var inp: INPUT
+  inp.type = INPUT_KEYBOARD
+  inp.ki.wVk = 0
+  inp.ki.dwExtraInfo = GetMessageExtraInfo()
+  inp.ki.time = 0
+  if sendKeyUp:
+    inp.ki.dwFlags = KEYEVENTF_KEYUP
+  inp.ki.dwFlags = inp.ki.dwFlags or KEYEVENTF_SCANCODE
+  inp.ki.wScan = uint16(MapVirtualKeyA(vkey, MAPVK_VK_TO_VSC_EX))
+  SendInput(1, inp, sizeof(inp))
+
+proc sendKey(rune: Rune, sendKeyUp:bool = false) =
+  var inp: INPUT
+  inp.type = INPUT_KEYBOARD
+  inp.ki.wVk = 0
+  inp.ki.dwExtraInfo = GetMessageExtraInfo()
+  inp.ki.time = 0
+
+  let vkey:SHORT = VkKeyScanEx(cast[WCHAR](rune), GetKeyboardLayout(0))
+  #echo fmt"{rune}: {LOBYTE(vkey)}-{HIBYTE(vkey)} {uint16(rune)} {ord(rune)}"
+
+  if (HIBYTE(vkey) and 1) == 1:
+    sendKey(VK_SHIFT, false)
+  if (HIBYTE(vkey) and 2) == 2:
+    sendKey(VK_CONTROL, false)
+  if (HIBYTE(vkey) and 4) == 4:
+    sendKey(VK_MENU, false)
+
+  if vkey == -1:
+    # No virtual key found, we'll send unicode instead
+    inp.ki.dwFlags = inp.ki.dwFlags or KEYEVENTF_UNICODE
+    inp.ki.wScan = uint16(rune)
+  else:
+    inp.ki.dwFlags = inp.ki.dwFlags or KEYEVENTF_SCANCODE  
+    let scanCode = MapVirtualKeyA(UINT(LOBYTE(vkey)), MAPVK_VK_TO_VSC_EX)
+    inp.ki.wScan = uint16(scanCode)
+  
+  SendInput(1, inp, sizeof(inp))
+  
+  if (HIBYTE(vkey) and 1) == 1:
+    sendKey(VK_SHIFT, true)
+  if (HIBYTE(vkey) and 2) == 2:
+    sendKey(VK_CONTROL, true)
+  if (HIBYTE(vkey) and 4) == 4:
+    sendKey(VK_MENU, true)
+  
+  inp.ki.dwFlags = inp.ki.dwFlags or KEYEVENTF_KEYUP
+  SendInput(1, inp, sizeof(inp))
+
 # Handles when menu item is selected from the list of windows
 frame.wEvent_Menu  do (event: wEvent):
   let mText = event.getMenuItem().mText
-  let hwnd = autoKey[mText]
+  let hwnd = autoKey[mText].hwnd
+  let class = autoKey[mText].className
+  let rect:RECT = autoKey[mText].rect
   let inputText = input.value
   input.value = ""
-  SetForegroundWindow(hwnd)
+  if class == "VMPlayerFrame":
+    echo "VMPlayerFrame"
+    let width = rect.right - rect.left
+    let height = rect.bottom - rect.top
+    let mx:int = int(width/2) + rect.left
+    let my:int = int(height/2) + rect.top
+    let dx = int((mx * 65536) / GetSystemMetrics(SM_CXSCREEN))
+    let dy = int((my * 65536) / GetSystemMetrics(SM_CYSCREEN))
+    var minp: INPUT
+    minp.type = INPUT_MOUSE
+    minp.mi.dX = dx
+    minp.mi.dY = dy
+    echo fmt"X: {dx} / Y: {dy} - {rect}"
+    minp.mi.mouseData = 0
+    minp.mi.dwFlags = MOUSEEVENTF_MOVE or MOUSEEVENTF_ABSOLUTE
+    SendInput(1, minp, sizeof(minp))
+    minp.mi.dwFlags = MOUSEEVENTF_LEFTUP
+    frame.minimize()
+    SetForegroundWindow(hwnd)
+    SendInput(1, minp, sizeof(minp))
+    minp.mi.dwFlags = MOUSEEVENTF_LEFTUP
+    SendInput(1, minp, sizeof(minp))
+  else:
+    frame.minimize()
+    SetForegroundWindow(hwnd)
   sleep(500)
   for chr in inputText.toRunes():
-    var inp: INPUT
-    inp.type = INPUT_KEYBOARD
-    inp.ki.wVk = 0
-    inp.ki.wScan = uint16(chr)
-    inp.ki.dwFlags = KEYEVENTF_UNICODE
-    var rtn:UINT = 0
-    while rtn == 0:
-      sleep(30)
-      rtn = SendInput(1, inp, sizeof(inp))
+    sleep(20)
+    sendKey(chr)
 
 # Callback function for EnumWindows - called for each window enumerated
 proc EnumWindowsHandler(hwnd: HWND, lparam: LPARAM): WINBOOL {.stdcall.} =
@@ -80,10 +156,17 @@ proc EnumWindowsHandler(hwnd: HWND, lparam: LPARAM): WINBOOL {.stdcall.} =
   if cast[bool](IsWindowVisible(hwnd)) and txtLength > 0:    
     GetWindowText(hwnd, wText, txtLength+1)
     GetClassName(hwnd, className, 256);  
+    var lprect: RECT
+    GetWindowRect(hwnd, lprect)
     if $className in @["Windows.UI.Core.CoreWindow", "ApplicationFrameWindow", "Progman", "CabinetWClass"]:
       return true
     autoMenu.append(wIdAny, $wText)
-    autoKey[$wText] = hwnd
+    var wnd: WindowObj
+    wnd.hwnd = hwnd
+    wnd.className = $className
+    wnd.rect = lprect
+    autoKey[$wText] = wnd
+    echo fmt"Window: {wText} - Class: {className}"
   VirtualFree(wText, cast[DWORD](txtLength+1), MEM_DECOMMIT)
   VirtualFree(wText, cast[DWORD](256), MEM_DECOMMIT)
   return  true
@@ -96,7 +179,7 @@ button.wEvent_Button do (event: wEvent):
   button.popupMenu(autoMenu)
 
 layout()
-#frame.center() # Center frame window in screen
+frame.center() # Center frame window in screen
 frame.show() # A frame is hidden on creation by default.
 
 app.mainLoop()
